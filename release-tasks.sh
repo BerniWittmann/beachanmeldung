f7cbbec68136cef92c10eef67263345e5cefebd8
BerniWittmann <wittmann_b@web.de>
2017-12-15 08:35:43 +0100

    update release tasks

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo "Starting Release Tasks"
+cd web/vueapp
+npm run build
+
+cd ../..
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py compilemessages
