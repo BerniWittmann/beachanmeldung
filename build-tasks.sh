49ff5989f7157ceda234c46000872ab1a57702c7
BerniWittmann <wittmann_b@web.de>
2017-12-17 10:08:35 +0100

    update build & release tasks

--- build-tasks.sh
+++ build-tasks.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 echo "Starting Build Tasks"
-python manage.py migrate
+cd web/vueapp
+npm install
+npm run build
+cd ../..
+
+python manage.py collectstatic --noinput
+python manage.py compilemessages
 echo "Completed Build Tasks"
--- release-tasks.sh
+++ release-tasks.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 echo "Starting Release Tasks"
-cd web/vueapp
-npm install
-npm run build
-cd ../..
-
-python manage.py collectstatic --noinput
-python manage.py compilemessages
+python manage.py migrate
 echo "Completed Release Tasks"
