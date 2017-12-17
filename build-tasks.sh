2bb6df08c1cf6b56012a1d51f83cdfc46fd1d1a3
BerniWittmann <wittmann_b@web.de>
2017-12-17 11:03:41 +0100

    update build tasks

--- build-tasks.sh
+++ build-tasks.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 echo "Starting Build Tasks"
-cd web/vueapp
-npm install
-npm run build
-cd ../..
 
-. venv/bin/activate
+echo "Bundling Assets"
+npm run build
+
+echo "Collect Statics"
 python manage.py collectstatic --noinput
+
+echo "Compile Translations"
 python manage.py compilemessages
+
 echo "Completed Build Tasks"
