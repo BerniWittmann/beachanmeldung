c0e948f24471cd4d6c20cc68434e93ea3beaf4dd
BerniWittmann <wittmann_b@web.de>
2018-01-08 12:57:46 +0100

    [backend] fix an error with https in emails

--- api/MailSender.py
+++ api/MailSender.py
@@ -76,7 +76,7 @@ class MailSender:
 
     def send_email(self, prefix, email, data):
         ctxt = dict(email=email,
-                    host='https://' + self.request.get_host(),
+                    host='http://' + self.request.get_host(),
                     iban=config('BANKING_IBAN', default=None),
                     bic=config('BANKING_BIC', default=None),
                     **data)
