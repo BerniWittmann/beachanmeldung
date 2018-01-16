cc8f73195a71a77de5517a5f3660f866087fb7a7
BerniWittmann <wittmann_b@web.de>
2018-01-16 12:32:16 +0100

    [backend] not set clickjacking header to embed views

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a Ch
 ### Changes
 
 ### Fixes
+* Not set X-Frame-Option header on embeds
 
 ### Breaks
 
--- embed/views.py
+++ embed/views.py
@@ -1,11 +1,13 @@
 # Create your views here.
 
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 from api.enums import TeamStateTypes
 from api.tournaments.models import Tournament
 
 
+@xframe_options_exempt
 def tournament_signup_list(request, pk):
     tournament = get_object_or_404(Tournament.objects.all(), pk=pk)
     teams = list(tournament.teams.filter(state=TeamStateTypes.signed_up))
